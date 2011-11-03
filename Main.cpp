7019b9648329b4471d3a1fa1ef4284e50cb993c0
nekorawesh <nekorawesh@gmail.com>
2011-11-03 18:10:00 +0000

    Initializing the project. Adding Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+	cout << "*** Welcome all members ***" << endl;
+	cout << "Let's start coding Angry Birds!!!" << endl;
+	return 0;
+}
