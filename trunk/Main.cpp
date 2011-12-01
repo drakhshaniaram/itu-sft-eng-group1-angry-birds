9e01b01f3302883de27eae4f543d12c09db84d24
nekorawesh@gmail.com <nekorawesh@gmail.com@2ae612df-75c4-cd8e-22a5-7b9b5f7283e2>
2011-12-01 11:59:33 +0000

    Inivitation second time!

--- trunk/Main.cpp
+++ trunk/Main.cpp
@@ -6,5 +6,7 @@ int main(int argc, char *argv[]){
 
 	cout << "*** Welcome all members ***" << endl;
 	cout << "Let's start coding Angry Birds!!!" << endl;
+	cout << "Guys! If we continue like this, we'll get a big ZERO at the"
+			 "End of the semester :))" << endl;
 	return 0;
 }
