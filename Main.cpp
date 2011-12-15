e21ee0e54c8e2c9efc8e5e17d41c57109dcee822
nekorawesh <nekorawesh@gmail.com>
2011-12-15 12:17:27 +0000

    Trying!!!

--- Main.cpp
+++ Main.cpp
@@ -8,5 +8,6 @@ int main(int argc, char *argv[]){
 	cout << "Let's start coding Angry Birds!!!" << endl;
 	cout << "Guys! If we continue like this, we'll get a big ZERO at the"
 			 "End of the semester :))" << endl;
+	cout << "Hi! How are you!!!" << endl;
 	return 0;
 }
